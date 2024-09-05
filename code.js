1f9c7c608c534efc4c0865a3045da8cfae0351b3
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-05 16:15:52 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function fib(n) {
-    return n;
+    if (n === 0) return [0];
+    if (n === 1) return [0,1];
+    let arr = [0, 1];
+    fibHelper(arr,n);
+    return arr;
+}
+function fibHelper(arr, max) {
+    var len = arr.length;
+    if (len === max + 1) return arr;
+    var len = arr.length;
+    arr.push(arr[len - 1] + arr[len - 2]);
+    fibHelper(arr, max);
 }
