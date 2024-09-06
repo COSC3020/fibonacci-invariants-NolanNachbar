7d73a59ec1e74eaf72c07c7b5c63adb9c9b484ff
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-06 10:20:35 -0600

    Update code.js

--- code.js
+++ code.js
@@ -12,34 +12,3 @@ function fibHelper(arr, max) {
     arr.push(arr[len - 1] + arr[len - 2]);
     fibHelper(arr, max);
 }
-
-const fs = require('fs');
-const jsc = require('jsverify');
-
-// eval(fs.readFileSync('code.js')+'');
-
-function fibTest(n) {
-    var fib_solns = [0];
-    if(n == 0) return fib_solns;
-    fib_solns[1] = 1;
-    if(n == 1) return fib_solns;
-    for(var i = 2; i <= n; i++) fib_solns[i] =
-        fib_solns[i-1] + fib_solns[i-2];
-    return fib_solns;
-}
-
-const test =
-    jsc.forall("nat", function(n) {
-        return JSON.stringify(fib(n)) ==
-            JSON.stringify(fibTest(n));
-    });
-
-// jsc.assert(test);
-
-// Print the result
-try {
-    jsc.assert(test);
-    console.log("Test passed");
-} catch (error) {
-    console.log("Test failed:", error.message);
-}
